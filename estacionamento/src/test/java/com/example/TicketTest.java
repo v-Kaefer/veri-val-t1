1c19eff5c661482b25ffa6172c8989acda1af82f
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2024-10-08 20:35:52 -0300

    Adição de três testes para os tickets

--- estacionamento/src/test/java/com/example/TicketTest.java
+++ estacionamento/src/test/java/com/example/TicketTest.java
@@ -1,9 +1,10 @@
 package com.example;
 
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import java.time.LocalDateTime;
 import org.junit.Test;
 
 public class TicketTest {
@@ -36,5 +37,31 @@ public class TicketTest {
         Ticket ticket = new Ticket(1, automovel, entrada);
         ticket.setPago(true);
         assertTrue(ticket.isPago());
+    }
+    
+    @Test
+    public void testFormatarEntrada() {
+        Automovel automovel = new Automovel("XYZ-9876", false);
+        LocalDateTime entrada = LocalDateTime.of(2023, 10, 1, 10, 0);
+        Ticket ticket = new Ticket(1, automovel, entrada);
+        assertEquals("01-10-2023 10:00", ticket.formatarEntrada());
+    }
+
+    @Test
+    public void testFormatarSaida() {
+        Automovel automovel = new Automovel("XYZ-9876", false);
+        LocalDateTime entrada = LocalDateTime.of(2023, 10, 1, 10, 0);
+        Ticket ticket = new Ticket(1, automovel, entrada);
+        LocalDateTime saida = LocalDateTime.of(2023, 10, 1, 12, 0);
+        ticket.setSaida(saida);
+        assertEquals("01-10-2023 12:00", ticket.formatarSaida());
+    }
+
+    @Test
+    public void testFormatarSaidaSemSaidaRegistrada() {
+        Automovel automovel = new Automovel("XYZ-9876", false);
+        LocalDateTime entrada = LocalDateTime.of(2023, 10, 1, 10, 0);
+        Ticket ticket = new Ticket(1, automovel, entrada);
+        assertEquals("Sem saída registrada", ticket.formatarSaida());
     }
 }
